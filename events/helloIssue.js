4a849803bc7822ae11d51f522e3b872e046aebdd
Christian Dupuis <cd@atomist.com>
2019-11-24 10:56:40 -0800

    Add event

---
+++ events/helloIssue.js
@@ -0,0 +1,10 @@
+const automation_client_1 = require("@atomist/automation-client");
+
+exports.HelloIssue = () => ({
+    subscription: automation_client_1.GraphQL.subscription("Issue"),
+    listener: async (e, ctx) => {
+        const issue = e.data.Issue[0];
+        await ctx.messageClient.addressChannels(`New issue ${issue.number}: ${issue.title}`, "cd");
+        return automation_client_1.Success;
+    },
+});
