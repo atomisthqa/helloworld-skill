4b201e1bb3b2ee91fd89477100203ae58bd94f25
Christian Dupuis <cd@atomist.com>
2019-11-24 12:15:58 -0800

    Update to addressUsers

--- events/helloIssue.js
+++ events/helloIssue.js
@@ -4,7 +4,7 @@ exports.Issue = () => ({
     subscription: automation_client_1.GraphQL.subscription("Issue"),
     listener: async (e, ctx) => {
         const issue = e.data.Issue[0];
-        await ctx.messageClient.addressChannels(`New issue ${issue.number}: ${issue.title}`, "cd");
+        await ctx.messageClient.addressUsers(`New issue ${issue.number}: ${issue.title}`, "cd");
         return automation_client_1.Success;
     },
 });
