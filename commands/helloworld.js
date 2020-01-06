b8730ebe0947245f0ddd1e7a37ee7b11580d93d7
Christian Dupuis <cd@atomist.com>
2020-01-06 22:32:33 +0100

    Update helloworld.js

--- commands/helloworld.js
+++ commands/helloworld.js
@@ -1,4 +1,6 @@
-exports.HelloWorld = () => async ci => {
-    const params = await ci.promptFor({name: {description: "Name of user"}});
-    await ci.addressChannels(`Hello ${params.name}`);
-};
+exports.HelloWorld = () => ({
+    listener: async (ci) => {
+        const params = await ci.promptFor({ name: { description: "Name of user" } });
+        await ci.addressChannels(`Hello ${params.name}`);
+    },
+});
